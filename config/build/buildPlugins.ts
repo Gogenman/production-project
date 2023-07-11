8456867f0acfc9aa9cfca7ed497fc979688bd122
mindkiller <gondza2828@gmail.com>
2023-07-11 19:18:44 +0300

    add wbk-bndl-anlzr

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,9 +1,9 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
-
-import webpack from 'webpack';
-import { BuildOptions } from './types/config';
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
+import webpack from 'webpack'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import { BuildOptions } from './types/config'
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
@@ -20,5 +20,8 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         }),
         // new webpack.HotModuleReplacementPlugin(),
         new ReactRefreshWebpackPlugin(),
-    ];
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
+        }),
+    ]
 }
